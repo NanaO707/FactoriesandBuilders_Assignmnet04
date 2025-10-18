0c413622c5eafe0e498ed4506333078441cfa939
RuthieGG <ruthggarcia99@gmail.com>
2025-10-18 16:51:04 -0400

    Added abstract factory and implementation classes

--- src/AbstractFactory.java
+++ src/AbstractFactory.java
@@ -1,11 +1,13 @@
-abstract class AbstractFactory
-{
+abstract class AbstractFactory {
     static PersianFactory persianFactory;
     static QuebecoisFactory quebecoisFactory;
     static ModernFactory modernFactory;
 
-    static AbstractFactory FactoryMethod(Product_Type type){
+    static AbstractFactory FactoryMethod(Product_Type type) {
         return null;
     }
+
     abstract House makeHouse();
+
     abstract Duplex makeDuplex();
+}
